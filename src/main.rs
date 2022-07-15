1130b02cd2437f27fea6d45929337c9d98f62ab6
ben <ben.strutt@hotmail.co.uk>
2022-07-15 21:19:36 +0100

    Get logging working and I guess spam the console with garbage

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,37 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, log::LogPlugin};
 
 fn main() {
-    wasm_logger::init(wasm_logger::Config::default());
+    // When building for WASM, print panics to the browser console
+    #[cfg(target_arch = "wasm32")]
+    console_error_panic_hook::set_once();
+
+    let logger_config = wasm_logger::Config::new(log::Level::Info);
+
+    wasm_logger::init(logger_config);
     log::debug!("App is starting");
+
     App::new()
-        .add_startup_system(add_people)
-        .add_system(greet_people)
-        .add_system(hello_world)
+        .add_plugins_with(DefaultPlugins, |group| group.disable::<LogPlugin>())
+        .add_plugin(HelloPlugin)
+        .add_startup_system(setup)
         .run();
 }
 
+fn setup(mut commands: Commands) {
+    log::info!("doop");
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.spawn_bundle(SpriteBundle {
+        sprite: Sprite {
+            color: Color::rgb(0.25, 0.25, 0.75),
+            custom_size: Some(Vec2::new(50.0, 50.0)),
+            ..default()
+        },
+        ..default()
+    });
+}
+
 fn hello_world() {
-    log::debug!("Hello World");
+    log::info!("Hello World");
 }
 
 #[derive(Component)]
@@ -28,6 +48,17 @@ fn add_people(mut commands: Commands) {
 
 fn greet_people(query: Query<&Name, With<Person>>) {
     for name in query.iter() {
-        log::debug!("hello {}!", name.0);
+        log::info!("hello {}!", name.0);
+    }
+}
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app
+            .add_startup_system(add_people)
+            .add_system(hello_world)
+            .add_system(greet_people);
     }
 }

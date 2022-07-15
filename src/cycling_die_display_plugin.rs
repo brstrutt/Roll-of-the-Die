08aca2a761f90a9d0b8f38d4fc8082d5e4494412
ben <ben.strutt@hotmail.co.uk>
2022-07-15 23:04:00 +0100

    Move the die animation to its own plugin

---
+++ src/cycling_die_display_plugin.rs
@@ -0,0 +1,48 @@
+use bevy::prelude::*;
+
+pub struct CyclingDieDisplayPlugin;
+
+impl Plugin for CyclingDieDisplayPlugin {
+    fn build(&self, app: &mut App) {
+        app
+            .add_startup_system(draw_die_face)
+            .add_system(animate_sprite);
+    }
+}
+
+
+#[derive(Component, Deref, DerefMut)]
+struct AnimationTimer(Timer);
+
+fn animate_sprite(
+    time: Res<Time>,
+    mut query: Query<(
+        &mut AnimationTimer,
+        &mut TextureAtlasSprite,
+        &Handle<TextureAtlas>,
+    )>,
+) {
+    for (mut timer, mut sprite, texture_atlas_handle) in query.iter_mut() {
+        timer.tick(time.delta());
+        if timer.just_finished() {
+            sprite.index = ((sprite.index) % 6) + 1;
+        }
+    }
+}
+
+fn draw_die_face(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>
+) {
+    let texture_handle = asset_server.load("resources/spritesheet.png");
+    let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(11.0, 11.0), 7, 5);
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+
+    commands.spawn_bundle(SpriteSheetBundle {
+        texture_atlas: texture_atlas_handle,
+        transform: Transform::from_scale(Vec3::splat(2.0)),
+        ..default()
+    })
+    .insert(AnimationTimer(Timer::from_seconds(0.5, true)));
+}
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use bevy::{prelude::*, log::LogPlugin};
 
 mod hello_plugin;
 mod blue_rect_plugin;
+mod cycling_die_display_plugin;
 
 fn main() {
     // When building for WASM, print panics to the browser console
@@ -18,48 +19,10 @@ fn main() {
         .add_startup_system(setup_2d_display)
         //.add_plugin(hello_plugin::HelloPlugin)
         //.add_plugin(blue_rect_plugin::BlueRectPlugin)
-        .add_startup_system(draw_die_face)
-        .add_system(animate_sprite)
+        .add_plugin(cycling_die_display_plugin::CyclingDieDisplayPlugin)
         .run();
 }
 
 fn setup_2d_display(mut commands: Commands) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-}
-
-#[derive(Component, Deref, DerefMut)]
-struct AnimationTimer(Timer);
-
-fn animate_sprite(
-    time: Res<Time>,
-    texture_atlases: Res<Assets<TextureAtlas>>,
-    mut query: Query<(
-        &mut AnimationTimer,
-        &mut TextureAtlasSprite,
-        &Handle<TextureAtlas>,
-    )>,
-) {
-    for (mut timer, mut sprite, texture_atlas_handle) in query.iter_mut() {
-        timer.tick(time.delta());
-        if timer.just_finished() {
-            sprite.index = ((sprite.index) % 6) + 1;
-        }
-    }
-}
-
-fn draw_die_face(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    mut texture_atlases: ResMut<Assets<TextureAtlas>>
-) {
-    let texture_handle = asset_server.load("resources/spritesheet.png");
-    let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(11.0, 11.0), 7, 5);
-    let texture_atlas_handle = texture_atlases.add(texture_atlas);
-
-    commands.spawn_bundle(SpriteSheetBundle {
-        texture_atlas: texture_atlas_handle,
-        transform: Transform::from_scale(Vec3::splat(2.0)),
-        ..default()
-    })
-    .insert(AnimationTimer(Timer::from_seconds(0.5, true)));
 }

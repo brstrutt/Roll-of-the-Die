9b3c89a4cc6337ac7d71899098d192cf0473ee04
ben <ben.strutt@hotmail.co.uk>
2022-07-17 17:21:01 +0100

    Game bGame broken but motion is not one big function anymore

--- src/die_plugin.rs
+++ src/die_plugin.rs
@@ -6,7 +6,7 @@ use bevy::{
     core::FixedTimestep,
 };
 
-use crate::{Collider, GRID_SIZE, PressurePlate, Spritesheet, GameState};
+use crate::{Collider, GRID_SIZE, PressurePlate, Spritesheet, GameState, PIXEL_SCALE};
 
 use super::direction::{
     *,
@@ -20,13 +20,15 @@ impl Plugin for DiePlugin {
             .add_startup_system_to_stage(StartupStage::PostStartup, setup)
             .add_system_set(
                 SystemSet::on_update(GameState::Playing)
-                    .with_system(move_die)
+                    .with_system(react_to_input)
+                    .with_system(tick_animation)
+                    .with_system(tick_motion)
             );
     }
 }
 
 pub const DIE_STARTING_POSITION: Vec3 = const_vec3!([0.0, 0.0, 1.0]);
-const MOVEMENT_COOLDOWN: f32 = 0.25;
+const DIE_SPEED: f32 = PIXEL_SCALE * GRID_SIZE; // Don't change this if you want your die to stay on screen
 
 fn setup(
     mut commands: Commands,
@@ -34,12 +36,6 @@ fn setup(
 ) {
     commands.spawn_bundle(DieBundle::new(&spritesheet.0));
 }
-
-#[derive(Component, Deref, DerefMut)]
-struct MovementCooldown(Timer);
-
-#[derive(Component, Deref, DerefMut)]
-struct AnimationFrameTimer(Timer);
 
 #[derive(PartialEq)]
 enum DieAnimation {
@@ -59,7 +55,6 @@ pub struct Die {
     pub hidden_number: usize,
     animation_state: DieAnimation,
     pub animation_direction: Direction,
-    animation_timer: AnimationFrameTimer,
     pub destination_translation: Vec3, // Store tile index, NOT absolute position
 }
 
@@ -67,7 +62,6 @@ pub struct Die {
 struct DieBundle {
     die: Die,
     collider: Collider,
-    movement_cooldown: MovementCooldown,
     #[bundle]
     sprite_bundle: SpriteSheetBundle,
 }
@@ -84,11 +78,9 @@ impl DieBundle {
                 hidden_number: 6,
                 animation_state: DieAnimation::None,
                 animation_direction: Direction::Up,
-                animation_timer: AnimationFrameTimer(Timer::from_seconds(MOVEMENT_COOLDOWN/4.0, false)),
                 destination_translation: DIE_STARTING_POSITION / GRID_SIZE,
             },
             collider: Collider,
-            movement_cooldown: MovementCooldown(Timer::from_seconds(MOVEMENT_COOLDOWN, false)),
             sprite_bundle: SpriteSheetBundle {
                 texture_atlas: texture_atlas_handle.clone(),
                 transform: Transform {
@@ -106,97 +98,104 @@ impl DieBundle {
     }
 }
 
-
-fn move_die(
-    time: Res<Time>,
+fn react_to_input(
     keyboard_input: Res<Input<KeyCode>>,
-    mut die_query: Query<(&mut Transform, &mut TextureAtlasSprite, &mut MovementCooldown, &mut Die)>,
+    mut die_query: Query<(&mut Transform, &mut TextureAtlasSprite, &mut Die)>,
     colliders_query: Query<
         & Transform,
         (With<Collider>,Without<Die>),
-    >,
-    mut pressure_plates_query: Query<
-        (&mut PressurePlate, &Transform),
-        (Without<Die>, Without<Collider>),
-    >,
+    >
+) {
+    let (die_transform, die_sprite, mut die) = die_query.single_mut();
+    if die.animation_state != DieAnimation::None { return; }
+    
+    let direction = keypress_to_direction(keyboard_input);
+    if direction.is_none() { return; }
+    let direction = direction.unwrap();
+
+    let new_position = die.destination_translation + translation_from_direction(&direction);
+
+    for collider in colliders_query.iter() {
+        if is_colliding(new_position * GRID_SIZE, collider.translation) {
+            return;
+        }
+    }
+
+    die.destination_translation = new_position;
+    rotate_die(&mut die, &direction);
+
+    die.animation_state = DieAnimation::Frame1;
+    die.animation_direction = direction.clone();
+}
+
+/*fn new_check_pressure_plates(
+    mut die_query: Query<(&mut Transform, &mut Die)>,
+    mut pressure_plates_query: Query<(&mut PressurePlate, &Transform)>
+) {
+    let (die_transform, die) = die_query.single_mut();
+    for (mut pressure_plate, pp_transform) in pressure_plates_query.iter_mut() {
+        if is_colliding(transform.translation, pp_transform.translation) && 
+            !pressure_plate.activated {
+            if die.face_number == pressure_plate.number {
+                pressure_plate.activated = true;
+                sprite.index = get_die_face_sprite_index(die.face_number) + 14;
+            }
+            else {
+                sprite.index = get_die_face_sprite_index(die.face_number) + 7;
+            }
+        }
+    }
+}*/
+
+fn tick_animation(
+    mut die_query: Query<(& Transform, &mut TextureAtlasSprite, &mut Die)>,
 ) {
     let (
         mut transform,
         mut sprite,
-        mut movement_cooldown,
-        mut die,) = die_query.single_mut();
-    
+        mut die) = die_query.single_mut();
+    if die.animation_state == DieAnimation::None { return; }
+
     let dist_to_dest = die.destination_translation * GRID_SIZE - transform.translation;
-    if die.animation_state != DieAnimation::None {
-        die.animation_timer.tick(time.delta());
-        if die.animation_timer.finished() {
-            match die.animation_state {
-                DieAnimation::Frame1 => {
-                    die.animation_state = DieAnimation::Frame2;
-                    die.animation_timer.reset();
-                    transform.translation += dist_to_dest / 3.0;
-                },
-                DieAnimation::Frame2 => {
-                    die.animation_state = DieAnimation::Frame3;
-                    die.animation_timer.reset();
-                    transform.translation += dist_to_dest / 2.0;
-                },
-                DieAnimation::Frame3 => {
-                    die.animation_state = DieAnimation::None;
-                    die.animation_timer.reset();
-                    transform.translation = die.destination_translation * GRID_SIZE;
-                },
-                DieAnimation::None => die.animation_state = DieAnimation::None,
-            }
-        }
 
-        match die.animation_state {
-            DieAnimation::Frame1 => sprite.index = get_die_animation_frame_index(1, die.animation_direction),
-            DieAnimation::Frame2 => sprite.index = get_die_animation_frame_index(2, die.animation_direction),
-            DieAnimation::Frame3 => sprite.index = get_die_animation_frame_index(3, die.animation_direction),
-            DieAnimation::None => sprite.index = get_die_face_sprite_index(die.face_number),
-        }
-    }
-    else
-    {
-        for (mut pressure_plate, pp_transform) in pressure_plates_query.iter_mut() {
-            if is_colliding(transform.translation, pp_transform.translation) && 
-                !pressure_plate.activated {
-                if die.face_number == pressure_plate.number {
-                    pressure_plate.activated = true;
-                    sprite.index = get_die_face_sprite_index(die.face_number) + 14;
-                }
-                else {
-                    sprite.index = get_die_face_sprite_index(die.face_number) + 7;
-                }
-            }
-        }
-    }
+    match dist_to_dest.length().abs() {
+        40.0.. => {
+            die.animation_state = DieAnimation::Frame1;
+            sprite.index = get_die_face_sprite_index(die.face_number);
+        },
+        28.0..=40.0 => {
+            die.animation_state = DieAnimation::Frame1;
+            sprite.index = get_die_animation_frame_index(1, die.animation_direction);
+        },
+        16.0..=28.0 => {
+            die.animation_state = DieAnimation::Frame2;
+            sprite.index = get_die_animation_frame_index(2, die.animation_direction);
+        },
+        4.0..=16.0 => {
+            die.animation_state = DieAnimation::Frame3;
+            sprite.index = get_die_animation_frame_index(3, die.animation_direction);
+        },
+        _ => {
+            die.animation_state = DieAnimation::None;
+            sprite.index = get_die_face_sprite_index(die.face_number);
+        },
 
-    movement_cooldown.tick(time.delta());
-    if movement_cooldown.finished() {
-        let direction = keypress_to_direction(keyboard_input);
-
-        if direction.is_none() { return; }
-        let direction = direction.unwrap();
-        let new_position = die.destination_translation + translation_from_direction(&direction);
-
-        for collider in colliders_query.iter() {
-            if is_colliding(new_position * GRID_SIZE, collider.translation) {
-                return;
-            }
-        }
-
-        die.destination_translation = new_position;
-        transform.translation += dist_to_dest / 4.0;
-
-        rotate_die(&mut die, &direction);
-        sprite.index = get_die_face_sprite_index(die.face_number);
-        movement_cooldown.reset();
-        die.animation_state = DieAnimation::Frame1;
-        die.animation_direction = direction.clone();
     }
 }
+
+fn tick_motion(
+    time: Res<Time>,
+    mut die_query: Query<(&mut Transform, &Die)>,
+) {
+    let (mut die_transform, die) = die_query.single_mut();
+
+    let velocity = (translation_from_direction(&die.animation_direction) * GRID_SIZE) / (DIE_SPEED * time.delta().as_secs_f32());
+    die_transform.translation += velocity;
+    if die.animation_state == DieAnimation::None { 
+        die_transform.translation = die.destination_translation * GRID_SIZE;
+    }
+}
+
 
 fn is_colliding(object1_pos: Vec3, object2_pos: Vec3) -> bool {
     let difference = object1_pos.sub(object2_pos);
